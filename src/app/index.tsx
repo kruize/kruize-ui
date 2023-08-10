357a0d79dc2a2976bde2817403cb05c4e386a622
bhanvimenghani <bmenghan@redhat.com>
2023-08-10 14:39:37 +0530

    fixes font style and changes to 'kruize'

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,6 @@ import '@patternfly/react-core/dist/styles/base.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AppLayout } from '@app/AppLayout/AppLayout';
 import { AppRoutes } from '@app/routes';
-import '@app/app.css';
 
 const App = () => {
   return (
