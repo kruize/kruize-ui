946666c423d7548247f697283ff3584f99202525
bhanvimenghani <bmenghan@redhat.com>
2023-12-11 17:55:05 +0530

    adds api integration & pagination

--- src/app/CentralConfig.tsx
+++ src/app/CentralConfig.tsx
@@ -2,40 +2,50 @@
 const isProduction = process.env.KRUIZE_UI_ENV === 'production';
 
 const getBaseUrl = () => {
-    if (isProduction) {
-        return "/api";
-    } else {
-        return `http://${process.env.CLUSTER_IP}:${process.env.KRUIZE_PORT}`;
-    }
-}
+  if (isProduction) {
+    return '/api';
+  } else {
+    return `http://${process.env.CLUSTER_IP}:${process.env.KRUIZE_PORT}`;
+  }
+};
 
 const getRecommendationsURLWithParams = (experiment_name: string, latest: string) => {
-    return getBaseUrl() + '/listRecommendations?experiment_name=' + experiment_name + '&latest=' + latest
-}
+  return getBaseUrl() + '/listRecommendations?experiment_name=' + experiment_name + '&latest=' + latest;
+};
 
 const getRecommendationsURL = () => {
-    return getBaseUrl() + '/listRecommendations';
-}
-
+  return getBaseUrl() + '/listRecommendations';
+};
 
 const getListExperimentsURL = () => {
-    return getBaseUrl() + '/listExperiments'
-}
+  return getBaseUrl() + '/listExperiments';
+};
+
+const getListExperimentsURLWithParams = (experiment_name: string) => {
+  return getBaseUrl() + '/listExperiments/getExperimentNames?search=' + experiment_name + '&page=1&limit=20';
+};
 
 const getHostname = () => {
-    if (isProduction) {
-        return window.location.hostname;
-    } else {
-        return process.env.CLUSTER_IP;
-    }
-}
+  if (isProduction) {
+    return window.location.hostname;
+  } else {
+    return process.env.CLUSTER_IP;
+  }
+};
 
 const getPort = () => {
-    if (isProduction) {
-        return window.location.port || (window.location.protocol === "https:" ? "443" : "80");
-    } else {
-        return process.env.KRUIZE_PORT;
-    }
-}
+  if (isProduction) {
+    return window.location.port || (window.location.protocol === 'https:' ? '443' : '80');
+  } else {
+    return process.env.KRUIZE_PORT;
+  }
+};
 
-export { getRecommendationsURLWithParams, getListExperimentsURL, getHostname, getPort, getRecommendationsURL };
+export {
+  getRecommendationsURLWithParams,
+  getListExperimentsURL,
+  getHostname,
+  getPort,
+  getRecommendationsURL,
+  getListExperimentsURLWithParams
+};
