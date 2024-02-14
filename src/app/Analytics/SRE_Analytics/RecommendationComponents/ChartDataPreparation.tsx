ee72f4075718efd904fd06eb7cf7925ae69bf374
bhanvimenghani <bmenghan@redhat.com>
2024-02-14 16:50:27 +0530

    char prep data func changes

--- src/app/Analytics/SRE_Analytics/RecommendationComponents/ChartDataPreparation.tsx
+++ src/app/Analytics/SRE_Analytics/RecommendationComponents/ChartDataPreparation.tsx
@@ -11,21 +11,7 @@ export function filterDataByTerm(data, givenDay, term) {
     throw new Error("Invalid term. Use 'short_term', 'medium_term', or 'long_term'.");
   }
 
-  const oneDayInMillis = 24 * 60 * 60 * 1000;
-  const givenDayMillis = Date.parse(givenDay);
-  const filteredData = {};
-
-  for (const key in data) {
-    if (data.hasOwnProperty(key)) {
-      const timestampMillis = Date.parse(key);
-
-      if (givenDayMillis - timestampMillis <= oneDayInMillis * numDays && givenDayMillis - timestampMillis >= 0) {
-        filteredData[key] = data[key];
-      }
-    }
-  }
-
-  return filteredData;
+  return filterLastNDayData(data, givenDay, numDays);
 }
 
 export function formatTimestamps(timestampsData) {
@@ -43,14 +29,14 @@ export function formatTimestamps(timestampsData) {
 
 export function formatNumber(input) {
   if (typeof input === 'number') {
-    return parseFloat(input.toFixed(3));
+    return parseFloat(input.toFixed(3)); // Format the number to 3 decimal places and convert to number
   } else if (!isNaN(input)) {
-    return parseFloat(Number(input).toFixed(3));
+    return parseFloat(Number(input).toFixed(3)); // Format the number to 3 decimal places and convert to number
   }
   return input;
 }
 export function addPlusSign(str) {
-  const number = parseFloat(str);
+  const number = parseFloat(str); // Try to convert the string to a number
 
   if (!isNaN(number) && isFinite(number)) {
     if (number >= 0) {
@@ -58,5 +44,30 @@ export function addPlusSign(str) {
     }
   }
 
-  return str;
+  return str; // Return the original string if it's not a non-negative number
+}
+
+export function filterLastNDayData(data, givenDay, numDays) {
+  const oneDayInMillis = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
+  const givenDayMillis = Date.parse(givenDay);
+  const filteredData = {};
+
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      const timestampMillis = Date.parse(key);
+      const isGivenTime =
+        new Date(key).getHours() === new Date(givenDay).getHours() &&
+        new Date(key).getMinutes() === new Date(givenDay).getMinutes();
+
+      if (
+        givenDayMillis - timestampMillis <= oneDayInMillis * numDays &&
+        givenDayMillis - timestampMillis >= 0 &&
+        !isGivenTime
+      ) {
+        filteredData[key] = data[key];
+      }
+    }
+  }
+
+  return filteredData;
 }
