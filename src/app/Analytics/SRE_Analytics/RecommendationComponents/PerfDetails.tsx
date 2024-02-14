77caf028ebc3a72f8e830424d00ed1bae04020d9
bhanvimenghani <bmenghan@redhat.com>
2024-02-14 17:10:34 +0530

    optional chaining to avoid error for null values

--- src/app/Analytics/SRE_Analytics/RecommendationComponents/PerfDetails.tsx
+++ src/app/Analytics/SRE_Analytics/RecommendationComponents/PerfDetails.tsx
@@ -24,42 +24,42 @@ const PerfDetails = (props: { recommendedData; currentData; chartData; day; endt
   const current_code = `resources: 
   requests: 
     memory: "${NumberFormat(props.currentData[0]?.requests?.memory?.amount)}${UnitFormat(
-    props.currentData[0]?.requests?.memory?.format
-  )}" 
+      props.currentData[0]?.requests?.memory?.format
+    )}" 
     cpu: "${NumberFormat(props.currentData[0]?.requests?.cpu?.amount)}" 
   limits: 
     memory: "${NumberFormat(props.currentData[0]?.limits?.memory?.amount)}${UnitFormat(
-    props.currentData[0]?.limits?.memory?.format
-  )}" 
+      props.currentData[0]?.limits?.memory?.format
+    )}" 
     cpu: "${NumberFormat(props.currentData[0]?.limits?.cpu?.amount)}"`;
 
   const recommended_code = `resources: 
   requests: 
     memory: "${NumberFormat(
-      props.recommendedData[0]?.recommendation_engines?.performance?.config.requests.memory.amount
+      props.recommendedData[0]?.recommendation_engines?.performance?.config?.requests?.memory?.amount
     )}${UnitFormat(
-    props.recommendedData[0]?.recommendation_engines?.performance?.config.requests.memory.format
-  )}"    # ${addPlusSign(
-    NumberFormat(props.recommendedData[0]?.recommendation_engines?.performance?.variation.requests.memory.amount)
-  )}${UnitFormat(props.recommendedData[0]?.recommendation_engines?.performance?.variation.requests.memory.format)}
+      props.recommendedData[0]?.recommendation_engines?.performance?.config?.requests?.memory?.format
+    )}"    # ${addPlusSign(
+      NumberFormat(props.recommendedData[0]?.recommendation_engines?.performance?.variation?.requests?.memory?.amount)
+    )}${UnitFormat(props.recommendedData[0]?.recommendation_engines?.performance?.variation?.requests?.memory?.format)}
     cpu: "${NumberFormat(
-      props.recommendedData[0]?.recommendation_engines?.performance?.config.requests.cpu.amount
+      props.recommendedData[0]?.recommendation_engines?.performance?.config?.requests?.cpu?.amount
     )}"            # ${addPlusSign(
-    NumberFormat(props.recommendedData[0]?.recommendation_engines?.performance?.variation.requests.cpu.amount)
-  )}
+      NumberFormat(props.recommendedData[0]?.recommendation_engines?.performance?.variation?.requests?.cpu?.amount)
+    )}
   limits: 
     memory: "${NumberFormat(
-      props.recommendedData[0]?.recommendation_engines?.performance?.config.limits.memory.amount
+      props.recommendedData[0]?.recommendation_engines?.performance?.config?.limits?.memory?.amount
     )}${UnitFormat(
-    props.recommendedData[0]?.recommendation_engines?.performance?.config.limits.memory.format
-  )}"    # ${addPlusSign(
-    NumberFormat(props.recommendedData[0]?.recommendation_engines?.performance?.variation.limits.memory.amount)
-  )}${UnitFormat(props.recommendedData[0]?.recommendation_engines?.performance?.variation.limits.memory.format)}   
+      props.recommendedData[0]?.recommendation_engines?.performance?.config?.limits?.memory?.format
+    )}"    # ${addPlusSign(
+      NumberFormat(props.recommendedData[0]?.recommendation_engines?.performance?.variation?.limits?.memory?.amount)
+    )}${UnitFormat(props.recommendedData[0]?.recommendation_engines?.performance?.variation?.limits?.memory?.format)}   
     cpu: "${NumberFormat(
-      props.recommendedData[0]?.recommendation_engines?.performance?.config.limits.cpu.amount
+      props.recommendedData[0]?.recommendation_engines?.performance?.config?.limits?.cpu?.amount
     )}"            # ${addPlusSign(
-    NumberFormat(props.recommendedData[0]?.recommendation_engines?.performance?.variation.limits.cpu.amount)
-  )}`;
+      NumberFormat(props.recommendedData[0]?.recommendation_engines?.performance?.variation?.limits?.cpu?.amount)
+    )}`;
 
   return (
     <PageSection variant={PageSectionVariants.light}>
