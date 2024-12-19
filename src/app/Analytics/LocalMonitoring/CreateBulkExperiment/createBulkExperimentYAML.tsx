31a06c7ba4e72a1c9301c2ee420257ded9d4b244
bhanvimenghani <menghanibhanvi@gmail.com>
2024-12-19 15:13:08 +0530

    updates createBulkExpYAML

--- src/app/Analytics/LocalMonitoring/CreateBulkExperiment/createBulkExperimentYAML.tsx
+++ src/app/Analytics/LocalMonitoring/CreateBulkExperiment/createBulkExperimentYAML.tsx
@@ -10,16 +10,10 @@ export default `{
       "namespace": [],
       "workload": [],
       "containers": [],
-      "labels": {
-        "key1": "value1",
-        "key2": "value2"
-      }
+      "labels": {}
     }
   },
-  "time_range": {
-    "start": "2024-11-01T00:00:00.000Z",
-    "end": "2024-11-15T23:59:59.000Z"
-  },
+  "time_range": {},
   "datasource": "subsitute_datasource_name",
   "webhook": {
     "url" : "http://127.0.0.1:8080/webhook"
